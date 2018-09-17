9d878d15a62183dc15de51ed04fcf0ae925ee27a
pkrinsky <pkrinsky@outlook.com>
2018-09-17 06:37:03 -0700

    added variable for delay

--- src/main/java/pacman/robot/Robot.java
+++ src/main/java/pacman/robot/Robot.java
@@ -3,25 +3,25 @@ package pacman.robot;
 import pacman.base.RobotBase;
 import pacman.base.RobotRunner;
 import pacman.base.Util;
-import pacman.commands.SpinGroup;
+import pacman.commands.*;
 
 public class Robot extends RobotBase {
 
-	
 	public static void log(String s) {
 		Util.log(s);
 	}	
 	
 	public static void main(String[] args) {
 
+		// how long to wait (in ms) before the next loop
+		long delay = 100;
+
 		// create a new robot
 		Robot robot = new Robot();
 		
-		// run the spin routine
-		RobotRunner.run(robot, new SpinGroup(), 1000);
+		// run the command group
+		RobotRunner.run(robot, new SpinGroup(), delay);
 		
-
-
 	}		
 
 }
